74320c8eb2c9e9de2d7705e30e0573bf9eaf4a5b
Lucadagostino0907 <ldagostino2@mail.austral.edu.ar>
2025-08-11 12:37:08 -0300

    Add files via upload

---
+++ proceso.py
@@ -0,0 +1,32 @@
+def math():
+    a = 57
+    b = 7
+    print(a + b) #suma
+
+    resta = a - b
+
+    print(resta)  #resta 
+
+    multiplicacion = a * b
+
+    print(multiplicacion) #multiplicacion
+
+    promedio = (a + b)/2
+
+    print(promedio) #promedio
+
+    cociente_int= a/b           #cociente entero
+    print(int(cociente_int))
+
+    resto = a % b 
+
+    print(resto)  #resto
+
+    cociente_float= a/b
+
+    print(float(cociente_float)) #cociente real
+
+    
+
+
+math()
